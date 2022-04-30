99138cd94f9a16c9b1a3976d34c79735c74c687f
Rebekah Mann Dauner <102560113+rmanndauner@users.noreply.github.com>
2022-04-30 11:53:24 -0500

    Update week5CodingAssignmentMarvel.js

--- week5CodingAssignmentMarvel.js
+++ week5CodingAssignmentMarvel.js
@@ -12,29 +12,18 @@
 //3)View list (view by title A-Z, genre)
 //4) Exit with prompt "Happy watching!"
 
-class Watch List {
-    constructor(movieTitle, genre, leadingActor){
-        this.movieTitle = movieTitle;
-        this.genre = genre; 
-        this.leadingActor = leadingActor; 
-    }
 
-    describe () {
-        return `${this.movieTitle} is a ${this.genre}. The leading actor is ${this.leadingActor}.`
-    }
-}
-
-class Genre {
-    constructor(genre) {
+class Movie{
+    constructor(title, genre){
+        this.title = title;
         this.genre = genre;
     }
 }
 
 class Menu {
     constructor() {
-        this.genre;
-        this.movieTitle;
-        this.leadingActor
+        this.watchList = [];
+        this.selectedMenu = null;
     }
 
     start() {
@@ -43,33 +32,56 @@ class Menu {
         while (selection != 0) {
             switch (selection) {
                 case '1':
-                    this.addMovie();
+                    this.createWatchList();
                     break;
                 case '2' :
-                    this.searchByGenre();
-                    break;
-                case '3':
                     this.deleteMovie();
                     break;
-                case'4':
-                    this.viewMovieList();
+                case '3':
+                    this.viewList();
                     break;
                 default : 
                     selection = 0;
             }
             selection = this.showMainMenu();
         }
+        //THIS WORKS! When I press 0, it says the alert
         alert('Happy Watching!');
     }
 
     showMainMenu() {
-        return prompt (`
+        return prompt (`Choose from the following list:
         0) Exit
-        1) Add Movie
-        2) Search by Genre
-        3) Delete Movie
-        4) View Watch List
+        1) Create Watch List
+        2) Delete Movie From List
+        3) View List
         `);
     }
 
+    createWatchList() {
+        let movieName = prompt('Enter the movie you would like to add.');
+        let movieGenre = prompt('Enter the movie genre.');
+        this.watchList.push(new Movie(movieName, movieGenre));
+    }
+    
+    deleteMovie() {
+        let index = prompt('Type the index of the movie you want to delete.');
+        this.watchList.splice(index, 1);
+    }
+    viewList() {
+        //nothing pops up here, I think it's because there is no array.
+        let movieList = '';
+        for (let i=0; i<this.watchList.length; i++) {
+            let movieDescription = `Title: ${this.watchList[i].title}, Genre: ${this.watchList[i].genre} \n`;
+            if (i == 0) {
+                movieList = movieDescription
+            } else {
+            movieList = movieList + movieDescription;
+            }
+        }
+        return prompt (`This is your Watch List:\n${movieList}`)
+    }
 }
+
+let menu = new Menu();
+menu.start();
